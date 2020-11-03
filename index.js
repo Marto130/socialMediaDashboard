f9e1a3e1ad29c75df3137cdd64bad9236c19b310
Martin <mlopesampietro@gmail.com>
2020-11-03 17:53:18 -0300

    .

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
 const checkbox= document.querySelector('#checkbox');
-
+console.log('funciona');
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
   checkbox.checked = true;
+} else {
+  checkbox.checked=false;
 }
 
 
 checkbox.addEventListener('change', (e)=>{
+  console.log(window.matchMedia('(prefers-color-scheme: dark)').matches);
+  console.log('Chequeado: ',checkbox.checked);
 
     if(!e.target.checked) {
       document.body.classList.remove('dark');
